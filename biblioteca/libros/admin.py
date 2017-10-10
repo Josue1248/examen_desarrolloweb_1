b2347b42d68761624fbf9016081651d4795d9ad7
Josue <josuem.inf@gmail.com>
2017-10-10 15:20:23 -0700

    Paso 11 :v

--- biblioteca/libros/admin.py
+++ biblioteca/libros/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import libros
 
 # Register your models here.
+admin.site.register(libros)
--- biblioteca/libros/views.py
+++ biblioteca/libros/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import libros
 
 # Create your views here.
+def Home(request):
+    print request
+    wb="Bienvenido"
+    wb2={"Usuario":"Miguel"}
+    return render(request,"home.html",{'wb':wb, 'wb2',wb2})
