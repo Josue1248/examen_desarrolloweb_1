6eec89e10840ab3e2f20bce116972814dc999c25
Josue <josuem.inf@gmail.com>
2017-10-10 15:11:29 -0700

    Parte 1 del examen (tarde)

---
+++ biblioteca/libros/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.conf import settings
+
+# Create your models here.
+class Libro(models.MOdel):
+    Nombre= CharField(max_length=50,)
+    Autor=CharField(max_length=50,)
+    Editorial= CharField(max_length=50,)
+    ISBN=CharField=(max_length=20,)
+    Precio=DecimalField(max_length=10,)
+    Creado=DataTimeField(auto_now_add=True)
+        def __str__(self):
+            return str(self.Libro)
